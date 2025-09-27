0c345820a8dbc60936f0905e0966d4179c5261d6
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-09-27 17:08:53 -0300

    Classes e enum prontos

---
+++ src/entities/Worker.java
@@ -0,0 +1,82 @@
+package entities;
+
+import entities.enums.WorkerLevel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Worker {
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalaray;
+
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker() {}
+
+    public Worker(String name, WorkerLevel level, Double baseSalaray, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalaray = baseSalaray;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalaray() {
+        return baseSalaray;
+    }
+
+    public void setBaseSalaray(Double baseSalaray) {
+        this.baseSalaray = baseSalaray;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract) {
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract) {
+        contracts.remove(contract);
+    }
+
+    public double income(int year, int month) {
+        double sum = baseSalaray;
+        Calendar calendar = Calendar.getInstance();
+        for (HourContract contract : contracts) {
+            calendar.setTime(contract.getDate());
+            int c_year = calendar.get(Calendar.YEAR);
+            int c_month = 1 + calendar.get(Calendar.MONTH);
+            if (year == c_year && month == c_month)
+                sum += contract.totalValue();
+        }
+        return sum;
+    }
+}
