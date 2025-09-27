bb8e94c6da1f8e6b194e260c0027e3ab1a6aa2f6
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-09-27 18:34:49 -0300

    Base da classe Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,20 @@
 package application;
 
+import java.io.ByteArrayInputStream;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
+        String fakeInputString = "";
+        ByteArrayInputStream fakeInput = new ByteArrayInputStream(fakeInputString.getBytes());
+        System.setIn(fakeInput);
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
     }
 }
